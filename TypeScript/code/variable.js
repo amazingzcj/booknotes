5dd8228475337bd89d68203099b91bc01810aefe
zhangchunjie <996146632@qq.com>
2019-08-30 17:26:14 +0800

    ts 基础 variable.ts

---
+++ TypeScript/code/variable.js
@@ -0,0 +1,24 @@
+var number_n = null;
+var string_n = null;
+var undefined_n = null;
+var null_n = null;
+var void_n = null;
+var number_un = undefined;
+var string_un = undefined;
+var undefined_un = undefined;
+var null_un = undefined;
+var void_un = undefined;
+var number_v = void ;
+var string_v = void ;
+var undefined_v = void ;
+var null_v = void ;
+var void_v = void ;
+var n = null;
+var un = undefined;
+var v = void ;
+var test1 = n;
+var test2 = un;
+var test3 = un;
+var test4 = v;
+// void 表示没有任何返回值的函数
+// null 和 undefined 可赋值给任意变量
---
+++ TypeScript/code/variable.ts
@@ -0,0 +1,29 @@
+let number_n: number = null;
+let string_n: string = null;
+let undefined_n: undefined = null;
+let null_n: null = null;
+let void_n: void = null;
+
+let number_un: number = undefined;
+let string_un: string = undefined;
+let undefined_un: undefined = undefined;
+let null_un: null = undefined;
+let void_un: void = undefined;
+
+let number_v: number = void;
+let string_v: string = void;
+let undefined_v: undefined = void;
+let null_v: null = void;
+let void_v: void = void;
+
+let n:null = null;
+let un:undefined = undefined;
+let v:void = void;
+
+let test1: number  = n;
+let test2: number  = un;
+let test3: string  = un;
+let test4: string  = v;
+
+// void 表示没有任何返回值的函数
+// null 和 undefined 可赋值给任意变量
